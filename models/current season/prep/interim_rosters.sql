db903f8abcde49827cc5a33885c59f488a96e061
Jacob Matson <matsonj@gmail.com>
2021-12-04 10:33:32 -0800

    adding simple projection and standings

---
+++ models/current season/prep/interim_rosters.sql
@@ -0,0 +1,10 @@
+WITH cte_max_win_rate AS (
+    SELECT MAX(wins) * 0.75 as "max_wins"
+    FROM jm_prep.rosters_with_winrate
+)
+SELECT rwg.*, 
+       rww.wins
+FROM {{ ref( 'rosters_with_gmscore' ) }} rwg 
+JOIN cte_max_win_rate mwr ON 1=1
+JOIN {{ ref( 'rosters_with_winrate' ) }} rww on rww.id = rwg.id 
+WHERE rww.wins > mwr.max_wins OR rwg.status <> 'FA'
