9c310843514d5ddb95453f094f335179d41cead8
Jacob Matson <matsonj@gmail.com>
2022-10-22 18:27:36 +0000

    more migration

--- transform/models/current season/prep/interim_rosters.sql
+++ transform/models/current season/prep/interim_rosters.sql
@@ -1,6 +1,6 @@
 WITH cte_max_win_rate AS (
     SELECT MAX(wins) * 0.75 as "max_wins"
-    FROM jm_prep.rosters_with_winrate
+    FROM {{ ref('rosters_with_winrate' ) }}
 )
 SELECT rwg.*, 
        rww.wins
